170923369c723f759402720fc05074a2c058e82a
StefanosTouf <stefetoufe@gmail.com>
2022-01-23 23:43:42 +0200

    finalize

--- src/main/kotlin/rflow/RFlow.kt
+++ src/main/kotlin/rflow/RFlow.kt
@@ -7,8 +7,8 @@ import arrow.core.Tuple6
 import arrow.core.Tuple7
 import arrow.fx.coroutines.parMap
 import kotlinx.coroutines.flow.*
-import reader.*
 import kotlin.experimental.ExperimentalTypeInference
+import reader.*
 
 abstract class RFlow<T : Any, U> {
     abstract val rflow: Reader<T, Flow<U>>
@@ -69,7 +69,7 @@ abstract class RFlow<T : Any, U> {
     }
 }
 
-fun <T : Any, U> RFlow<T, Flow<U>>.genFlattenFlow(concurrency: Int = DEFAULT_CONCURRENCY): RFlow<T, U> =
+fun <T : Any, U> RFlow<T, Flow<U>>.genFlattenFlow(concurrency: Int): RFlow<T, U> =
     this.genMapFlow { _, fl -> fl.flattenMerge(concurrency) }
 
 class RFlowU<U>(override val rflow: Reader<Unit, Flow<U>>) : RFlow<Unit, U>() {
@@ -100,7 +100,7 @@ class RFlowU<U>(override val rflow: Reader<Unit, Flow<U>>) : RFlow<Unit, U>() {
     inline fun catch(crossinline action: suspend (Unit, Throwable) -> Unit): RFlowU<U> =
         from(this.genCatch(action))
 
-    fun <T : Any> requires(): RFlow1<T, U> =
+    fun <T : Any> require(): RFlow1<T, U> =
         RFlow1(this.rflow.local { })
 
     fun fulfill(): Flow<U> =
@@ -117,6 +117,9 @@ class RFlowU<U>(override val rflow: Reader<Unit, Flow<U>>) : RFlow<Unit, U>() {
 
 fun <U> RFlowU<Flow<U>>.flattenFlow(concurrency: Int = DEFAULT_CONCURRENCY): RFlowU<U> =
     RFlowU.from(this.genFlattenFlow(concurrency))
+
+fun <U> RFlowU<RFlowU<U>>.flattenMerge(concurrency: Int = DEFAULT_CONCURRENCY): RFlowU<U> =
+    RFlowU.from(this.genFlatMap(concurrency) { _, fl -> fl })
 
 class RFlow1<T : Any, U>(override val rflow: Reader<T, Flow<U>>) : RFlow<T, U>() {
 
@@ -147,11 +150,11 @@ class RFlow1<T : Any, U>(override val rflow: Reader<T, Flow<U>>) : RFlow<T, U>()
     inline fun catch(crossinline action: suspend (T, Throwable) -> Unit): RFlow1<T, U> =
         from(this.genCatch(action))
 
-    fun <T2 : Any> requires(): RFlow2<T2, T, U> =
+    fun <T2 : Any> require(): RFlow2<T2, T, U> =
         RFlow2(this.rflow.local { rt: Pair<T2, T> -> rt.second })
 
-    fun fulfill(t1: T): Flow<U> =
-        this.rflow.runReader(t1)
+    fun fulfill(t: T): Flow<U> =
+        this.rflow.runReader(t)
 
     companion object {
         fun <T : Any, U> from(rf: RFlow<T, U>): RFlow1<T, U> =
@@ -162,9 +165,11 @@ class RFlow1<T : Any, U>(override val rflow: Reader<T, Flow<U>>) : RFlow<T, U>()
 fun <T : Any, U> RFlow1<T, Flow<U>>.flattenFlow(concurrency: Int = DEFAULT_CONCURRENCY): RFlow1<T, U> =
     RFlow1.from(this.genFlattenFlow(concurrency))
 
+fun <T : Any, U> RFlow1<T, RFlow1<T, U>>.flattenMerge(concurrency: Int = DEFAULT_CONCURRENCY): RFlow1<T, U> =
+    RFlow1.from(this.genFlatMap(concurrency) { _, fl -> fl })
+
 class RFlow2<T : Any, T2 : Any, U>(override val rflow: Reader<Pair<T, T2>, Flow<U>>) :
     RFlow<Pair<T, T2>, U>() {
-
 
     @OptIn(ExperimentalTypeInference::class)
     inline fun <R> transform(
@@ -202,20 +207,27 @@ class RFlow2<T : Any, T2 : Any, U>(override val rflow: Reader<Pair<T, T2>, Flow<
         fromPairs(this.genCatch(action))
 
 
-    fun <T3 : Any> requires(): RFlow3<T3, T, T2, U> =
+    fun <T3 : Any> require(): RFlow3<T3, T, T2, U> =
         RFlow3(this.rflow.local { rt: Triple<T3, T, T2> -> Pair(rt.second, rt.third) })
 
-    fun fulfill(t1: T, t2: T2): Flow<U> =
-        this.rflow.runReader(Pair(t1, t2))
+    fun fulfill(t1: T): RFlow1<T2, U> =
+        RFlow1(this.rflow.local { rt: T2 ->
+            Pair(t1, rt)
+        })
+
 
     companion object {
         fun <T : Any, T2 : Any, U> fromPairs(rf: RFlow<Pair<T, T2>, U>): RFlow2<T, T2, U> =
             RFlow2(rf.rflow.local { it })
     }
+
 }
 
 fun <T : Any, T2 : Any, U> RFlow2<T, T2, Flow<U>>.flattenFlow(concurrency: Int = DEFAULT_CONCURRENCY): RFlow2<T, T2, U> =
     RFlow2.fromPairs(this.genFlattenFlow(concurrency))
+
+fun <T : Any, T2 : Any, U> RFlow2<T, T2, RFlow2<T, T2, U>>.flattenMerge(concurrency: Int = DEFAULT_CONCURRENCY): RFlow2<T, T2, U> =
+    RFlow2.fromPairs(this.genFlatMap(concurrency) { _, fl -> fl })
 
 class RFlow3<T : Any, T2 : Any, T3 : Any, U>(override val rflow: Reader<Triple<T, T2, T3>, Flow<U>>) :
     RFlow<Triple<T, T2, T3>, U>() {
@@ -248,13 +260,20 @@ class RFlow3<T : Any, T2 : Any, T3 : Any, U>(override val rflow: Reader<Triple<T
     inline fun catch(crossinline action: suspend (Triple<T, T2, T3>, Throwable) -> Unit): RFlow3<T, T2, T3, U> =
         from(this.genCatch(action))
 
-    fun <T4 : Any> requires(): RFlow4<T4, T, T2, T3, U> =
+    fun <T4 : Any> require(): RFlow4<T4, T, T2, T3, U> =
         RFlow4(this.rflow.local { rt: Tuple4<T4, T, T2, T3> ->
             Triple(rt.second, rt.third, rt.fourth)
         })
 
-    fun fulfill(t1: T, t2: T2, t3: T3): Flow<U> =
-        this.rflow.runReader(Triple(t1, t2, t3))
+    fun fulfill(t1: T): RFlow2<T2, T3, U> =
+        RFlow2(this.rflow.local { rt: Pair<T2, T3> ->
+            Triple(t1, rt.first, rt.second)
+        })
+
+    fun fulfillLast(t3: T3): RFlow2<T, T2, U> =
+        RFlow2(this.rflow.local { (t, t2): Pair<T, T2> ->
+            Triple(t, t2, t3)
+        })
 
     companion object {
         fun <T : Any, T2 : Any, T3 : Any, U> fromPairs(rf: RFlow<Pair<T, Pair<T2, T3>>, U>): RFlow3<T, T2, T3, U> =
@@ -269,6 +288,10 @@ class RFlow3<T : Any, T2 : Any, T3 : Any, U>(override val rflow: Reader<Triple<T
 
 fun <T : Any, T2 : Any, T3 : Any, U> RFlow3<T, T2, T3, Flow<U>>.flattenFlow(concurrency: Int = DEFAULT_CONCURRENCY): RFlow3<T, T2, T3, U> =
     RFlow3.from(this.genFlattenFlow(concurrency))
+
+fun <T : Any, T2 : Any, T3 : Any, U> RFlow3<T, T2, T3, RFlow3<T, T2, T3, U>>.flattenMerge(
+        concurrency: Int = DEFAULT_CONCURRENCY): RFlow3<T, T2, T3, U> =
+    RFlow3.from(this.genFlatMap(concurrency) { _, fl -> fl })
 
 class RFlow4<T : Any, T2 : Any, T3 : Any, T4 : Any, U>(override val rflow: Reader<Tuple4<T, T2, T3, T4>, Flow<U>>) :
     RFlow<Tuple4<T, T2, T3, T4>, U>() {
@@ -301,10 +324,17 @@ class RFlow4<T : Any, T2 : Any, T3 : Any, T4 : Any, U>(override val rflow: Reade
     inline fun catch(crossinline action: suspend (Tuple4<T, T2, T3, T4>, Throwable) -> Unit): RFlow4<T, T2, T3, T4, U> =
         from(this.genCatch(action))
 
-    fun fulfill(t1: T, t2: T2, t3: T3, t4: T4): Flow<U> =
-        this.rflow.runReader(Tuple4(t1, t2, t3, t4))
+    fun fulfill(t1: T): RFlow3<T2, T3, T4, U> =
+        RFlow3(this.rflow.local { rt: Triple<T2, T3, T4> ->
+            Tuple4(t1, rt.first, rt.second, rt.third)
+        })
 
-    fun <T5 : Any> requires(): RFlow5<T5, T, T2, T3, T4, U> =
+    fun fulfillLast(t4: T4): RFlow3<T, T2, T3, U> =
+        RFlow3(this.rflow.local { (t, t2, t3): Triple<T, T2, T3> ->
+            Tuple4(t, t2, t3, t4)
+        })
+
+    fun <T5 : Any> require(): RFlow5<T5, T, T2, T3, T4, U> =
         RFlow5(this.rflow.local { rt: Tuple5<T5, T, T2, T3, T4> ->
             Tuple4(rt.second, rt.third, rt.fourth, rt.fifth)
         })
@@ -325,6 +355,10 @@ class RFlow4<T : Any, T2 : Any, T3 : Any, T4 : Any, U>(override val rflow: Reade
 fun <T : Any, T2 : Any, T3 : Any, T4 : Any, U> RFlow4<T, T2, T3, T4, Flow<U>>.flattenFlow(
         concurrency: Int = DEFAULT_CONCURRENCY): RFlow4<T, T2, T3, T4, U> =
     RFlow4.from(this.genFlattenFlow(concurrency))
+
+fun <T : Any, T2 : Any, T3 : Any, T4 : Any, U> RFlow4<T, T2, T3, T4, RFlow4<T, T2, T3, T4, U>>.flattenMerge(
+        concurrency: Int = DEFAULT_CONCURRENCY): RFlow4<T, T2, T3, T4, U> =
+    RFlow4.from(this.genFlatMap(concurrency) { _, fl -> fl })
 
 class RFlow5<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, U>(override val rflow: Reader<Tuple5<T, T2, T3, T4, T5>, Flow<U>>) :
     RFlow<Tuple5<T, T2, T3, T4, T5>, U>() {
@@ -357,10 +391,13 @@ class RFlow5<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, U>(override val rf
     inline fun catch(crossinline action: suspend (Tuple5<T, T2, T3, T4, T5>, Throwable) -> Unit): RFlow5<T, T2, T3, T4, T5, U> =
         from(this.genCatch(action))
 
-    fun fulfill(t1: T, t2: T2, t3: T3, t4: T4, t5: T5): Flow<U> =
-        this.rflow.runReader(Tuple5(t1, t2, t3, t4, t5))
+    fun fulfill(t1: T): RFlow4<T2, T3, T4, T5, U> =
+        RFlow4(this.rflow.local { rt: Tuple4<T2, T3, T4, T5> ->
+            Tuple5(t1, rt.first, rt.second, rt.third, rt.fourth)
+        })
 
-    fun <T6 : Any> requires(): RFlow6<T6, T, T2, T3, T4, T5, U> =
+
+    fun <T6 : Any> require(r: Requirement<T6>): RFlow6<T6, T, T2, T3, T4, T5, U> =
         RFlow6(this.rflow.local { rt: Tuple6<T6, T, T2, T3, T4, T5> ->
             Tuple5(rt.second, rt.third, rt.fourth, rt.fifth, rt.sixth)
         })
@@ -381,6 +418,10 @@ class RFlow5<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, U>(override val rf
 fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, U> RFlow5<T, T2, T3, T4, T5, Flow<U>>.flattenFlow(
         concurrency: Int = DEFAULT_CONCURRENCY): RFlow5<T, T2, T3, T4, T5, U> =
     RFlow5.from(this.genFlattenFlow(concurrency))
+
+fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, U> RFlow5<T, T2, T3, T4, T5, RFlow5<T, T2, T3, T4, T5, U>>.flattenMerge(
+        concurrency: Int = DEFAULT_CONCURRENCY): RFlow5<T, T2, T3, T4, T5, U> =
+    RFlow5.from(this.genFlatMap(concurrency) { _, fl -> fl })
 
 class RFlow6<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, U>(override val rflow: Reader<Tuple6<T, T2, T3, T4, T5, T6>, Flow<U>>) :
     RFlow<Tuple6<T, T2, T3, T4, T5, T6>, U>() {
@@ -413,8 +454,20 @@ class RFlow6<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, U>(overr
     inline fun catch(crossinline action: suspend (Tuple6<T, T2, T3, T4, T5, T6>, Throwable) -> Unit): RFlow6<T, T2, T3, T4, T5, T6, U> =
         from(this.genCatch(action))
 
-    fun fulfill(t1: T, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6): Flow<U> =
-        this.rflow.runReader(Tuple6(t1, t2, t3, t4, t5, t6))
+    fun fulfill(t1: T): RFlow5<T2, T3, T4, T5, T6, U> =
+        RFlow5(this.rflow.local { rt: Tuple5<T2, T3, T4, T5, T6> ->
+            Tuple6(t1, rt.first, rt.second, rt.third, rt.fourth, rt.fifth)
+        })
+
+    fun fulfillLast(t6: T6): RFlow5<T, T2, T3, T4, T5, U> =
+        RFlow5(this.rflow.local { (t, t2, t3, t4, t5): Tuple5<T, T2, T3, T4, T5> ->
+            Tuple6(t, t2, t3, t4, t5, t6)
+        })
+
+    fun <T7 : Any> require(r: Requirement<T7>): RFlow7<T7, T, T2, T3, T4, T5, T6, U> =
+        RFlow7(this.rflow.local { rt: Tuple7<T7, T, T2, T3, T4, T5, T6> ->
+            Tuple6(rt.second, rt.third, rt.fourth, rt.fifth, rt.sixth, rt.seventh)
+        })
 
     companion object {
         fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, U> from(rf: RFlow<Tuple6<T, T2, T3, T4, T5, T6>, U>): RFlow6<T, T2, T3, T4, T5, T6, U> =
@@ -425,6 +478,10 @@ class RFlow6<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, U>(overr
 fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, U> RFlow6<T, T2, T3, T4, T5, T6, Flow<U>>.flattenFlow(
         concurrency: Int = DEFAULT_CONCURRENCY): RFlow6<T, T2, T3, T4, T5, T6, U> =
     RFlow6.from(this.genFlattenFlow(concurrency))
+
+fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, U> RFlow6<T, T2, T3, T4, T5, T6, RFlow6<T, T2, T3, T4, T5, T6, U>>.flattenMerge(
+        concurrency: Int = DEFAULT_CONCURRENCY): RFlow6<T, T2, T3, T4, T5, T6, U> =
+    RFlow6.from(this.genFlatMap(concurrency) { _, fl -> fl })
 
 class RFlow7<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, T7 : Any, U>(override val rflow: Reader<Tuple7<T, T2, T3, T4, T5, T6, T7>, Flow<U>>) :
     RFlow<Tuple7<T, T2, T3, T4, T5, T6, T7>, U>() {
@@ -457,8 +514,15 @@ class RFlow7<T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, T7 : Any
     inline fun catch(crossinline action: suspend (Tuple7<T, T2, T3, T4, T5, T6, T7>, Throwable) -> Unit): RFlow7<T, T2, T3, T4, T5, T6, T7, U> =
         from(this.genCatch(action))
 
-    fun fulfill(t1: T, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7): Flow<U> =
-        this.rflow.runReader(Tuple7(t1, t2, t3, t4, t5, t6, t7))
+    fun fulfill(t1: T): RFlow6<T2, T3, T4, T5, T6, T7, U> =
+        RFlow6(this.rflow.local { rt: Tuple6<T2, T3, T4, T5, T6, T7> ->
+            Tuple7(t1, rt.first, rt.second, rt.third, rt.fourth, rt.fifth, rt.sixth)
+        })
+
+    fun fulfillLast(t7: T7): RFlow6<T, T2, T3, T4, T5, T6, U> =
+        RFlow6(this.rflow.local { (t, t2, t3, t4, t5, t6): Tuple6<T, T2, T3, T4, T5, T6> ->
+            Tuple7(t, t2, t3, t4, t5, t6, t7)
+        })
 
     companion object {
         fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, T7 : Any, U> from(rf: RFlow<Tuple7<T, T2, T3, T4, T5, T6, T7>, U>): RFlow7<T, T2, T3, T4, T5, T6, T7, U> =
@@ -470,12 +534,28 @@ fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, T7 : Any, U> RFl
         concurrency: Int = DEFAULT_CONCURRENCY): RFlow7<T, T2, T3, T4, T5, T6, T7, U> =
     RFlow7.from(this.genFlattenFlow(concurrency))
 
+fun <T : Any, T2 : Any, T3 : Any, T4 : Any, T5 : Any, T6 : Any, T7 : Any, U> RFlow7<T, T2, T3, T4, T5, T6, T7, RFlow7<T, T2, T3, T4, T5, T6, T7, U>>.flattenMerge(
+        concurrency: Int = DEFAULT_CONCURRENCY): RFlow7<T, T2, T3, T4, T5, T6, T7, U> =
+    RFlow7.from(this.genFlatMap(concurrency) { _, fl -> fl })
 
-fun <U> Flow<U>.asR(): RFlowU<U> =
-    RFlowU.from(this)
+class Requirement<A>()
 
-fun <T : Any, U> Flow<U>.requires(): RFlow1<T, U> =
-    RFlow.pure(this)
+fun <A> require() = Requirement<A>()
 
-fun <U> rFlowOf(vararg elements: U): RFlowU<U> = RFlowU.from(elements.asFlow())
-fun <U> rFlowOf(value: U): RFlowU<U> = RFlowU.from(flowOf(value))
+fun <T : Any, U> Flow<U>.asRFlow(r: Requirement<T>): RFlow1<T, U> =
+    RFlow1.from(RFlow.pure(this))
+
+fun <T : Any, U> rFlowOf(r: Requirement<T>, vararg elements: U): RFlow1<T, U> =
+    RFlow1.from(RFlow.pure(elements.asFlow()))
+
+object InitOperators {
+    fun <T : Any, U> asRFlow(fl: Flow<U>, r: Requirement<T>): RFlow1<T, U> =
+        RFlow1.from(RFlow.pure(fl))
+
+    fun <T : Any, U> rFlowOf(r: Requirement<T>, elements: List<U>): RFlow1<T, U> =
+        RFlow1.from(RFlow.pure(elements.asFlow()))
+
+    fun <T : Any, U> rFlowOf(r: Requirement<T>, elements: U): RFlow1<T, U> = RFlow1.from(RFlow.pure(
+        flowOf(elements)))
+
+}
